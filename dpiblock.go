306ffbd842db73f507f9f1629820446667c4aaf9
Simone Basso <bassosimone@gmail.com>
2023-08-25 12:12:22 +0200

    feat: implement sending EOF for offending SNIs
    Useful to move forward with https://github.com/ooni/probe/issues/1803

--- dpiblock.go
+++ dpiblock.go
@@ -279,3 +279,80 @@ func (r *DPISpoofDNSResponse) Filter(
 
 	return policy, true
 }
+
+// DPICloseConnectionForTLSSNI is a [DPIRule] that spoofs a FIN|ACK TCP segment
+// after it sees a given TLS SNI. The zero value is invalid; please, fill
+// all the fields marked as MANDATORY.
+//
+// Note: this rule assumes that there is a router in the path that
+// can generate a spoofed RST segment. If there is no router in the
+// path, no RST segment will ever be generated.
+//
+// Note: this rule relies on a race condition. For consistent results
+// you MUST set some delay in the router<->server link.
+type DPICloseConnectionForTLSSNI struct {
+	// Logger is the MANDATORY logger.
+	Logger Logger
+
+	// SNI is the MANDATORY offending SNI.
+	SNI string
+}
+
+var _ DPIRule = &DPICloseConnectionForTLSSNI{}
+
+// Filter implements DPIRule
+func (r *DPICloseConnectionForTLSSNI) Filter(
+	direction DPIDirection, packet *DissectedPacket) (*DPIPolicy, bool) {
+	// short circuit for the return path
+	if direction != DPIDirectionClientToServer {
+		return nil, false
+	}
+
+	// short circuit for UDP packets
+	if packet.TransportProtocol() != layers.IPProtocolTCP {
+		return nil, false
+	}
+
+	// short circuit in case of misconfiguration
+	if r.SNI == "" {
+		return nil, false
+	}
+
+	// try to obtain the SNI
+	sni, err := packet.parseTLSServerName()
+	if err != nil {
+		return nil, false
+	}
+
+	// if the packet is not offending, accept it
+	if sni != r.SNI {
+		return nil, false
+	}
+
+	// generate the frame to spoof
+	spoofed, err := reflectDissectedTCPSegmentWithFINACKFlag(packet)
+	if err != nil {
+		return nil, false
+	}
+
+	// tell the user we're asking the router to FIN the flow.
+	r.Logger.Infof(
+		"netem: dpi: asking to send FIN|ACK to flow %s:%d %s:%d/%s because SNI==%s",
+		packet.SourceIPAddress(),
+		packet.SourcePort(),
+		packet.DestinationIPAddress(),
+		packet.DestinationPort(),
+		packet.TransportProtocol(),
+		sni,
+	)
+
+	// make sure the router knows it should spoof
+	policy := &DPIPolicy{
+		Delay:   0,
+		Flags:   FrameFlagSpoof,
+		PLR:     0,
+		Spoofed: [][]byte{spoofed},
+	}
+
+	return policy, true
+}
